08ed461b53315fe50bad09926448be36c84f6f35
Ikenna Okpo <ikenna.okpo@aurea.com>
2021-08-01 21:08:12 +0100

    added try...catch

--- index.js
+++ index.js
@@ -2,35 +2,39 @@ import loadJsonFile from "load-json-file"
 import parseTime from "./src/utils/parseTime.js";
 
 const get_all_sensors = async (equipment_id, start_time, end_time) => {
-    const output = [];
+    try {
+        const output = [];
     
-    const parsedStartTime = parseTime(start_time);
-    const parsedEndTime = parseTime(end_time);
+        const parsedStartTime = parseTime(start_time);
+        const parsedEndTime = parseTime(end_time);
 
-    const data = await loadJsonFile('./src/data.json');
-    const equipmentsMapping = data['EquipmentsMapping'];
+        const data = await loadJsonFile('./src/data.json');
+        const equipmentsMapping = data['EquipmentsMapping'];
 
-    const sensorMappings = equipmentsMapping[equipment_id];
-        
-    sensorMappings?.forEach((mapping) => {
-        let mappingStartTime = parseTime(mapping.start_time);
-        let mappingEndTime = parseTime(mapping.end_time);
+        const sensorMappings = equipmentsMapping[equipment_id];
+            
+        sensorMappings?.forEach((mapping) => {
+            let mappingStartTime = parseTime(mapping.start_time);
+            let mappingEndTime = parseTime(mapping.end_time);
 
-        if(parsedStartTime > mappingEndTime || parsedEndTime < mappingStartTime) {
-            return;
-        } else {
-            const outputStartTime = mappingStartTime < parsedStartTime ? start_time : mapping.start_time;
-            const outputEndTime = parsedEndTime < mappingEndTime ? end_time : mapping.end_time;
+            if(parsedStartTime > mappingEndTime || parsedEndTime < mappingStartTime) {
+                return;
+            } else {
+                const outputStartTime = mappingStartTime < parsedStartTime ? start_time : mapping.start_time;
+                const outputEndTime = parsedEndTime < mappingEndTime ? end_time : mapping.end_time;
 
-            const outputItem = {
-                [mapping._id]: `("${outputStartTime}", "${outputEndTime}")`
+                const outputItem = {
+                    [mapping._id]: `("${outputStartTime}", "${outputEndTime}")`
+                }
+                output.push(outputItem);
             }
-            output.push(outputItem);
-        }
 
-    })
+        })
 
-    return output;
+        return output;
+    } catch (e) {
+        console.log(e)
+    }
 
 }
 
